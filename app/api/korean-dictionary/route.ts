1b43473644520e9ba713c3e7ee5a374fcee5dad0
jaekeun Choi <jaek1783@gmail.com>
2025-01-08 14:16:55 +0900

    오류 수정중

--- app/api/korean-dictionary/route.ts
+++ app/api/korean-dictionary/route.ts
@@ -3,12 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 // 표준국어대사전 데이터 요청(json)
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("query");
-  console.log("query", query);
+  const word = searchParams.get("word");
+  console.log("word", word);
 
   const params = new URLSearchParams({
     key: process.env.NEXT_PUBLIC_STDICT_API_KEY!, // 환경변수에 타입 단언 추가
-    q: query ?? "", // null 병합 연산자로 기본값 처리
+    q: word ?? "", // null 병합 연산자로 기본값 처리
     req_type: "json"
   });
   console.log("params", params);
