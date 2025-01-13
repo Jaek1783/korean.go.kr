c9cdca172bdfeebe998fc6db8633bd9e8bfad6e5
jaekeun Choi <jaek1783@gmail.com>
2025-01-13 14:28:06 +0900

    css 변경

--- components/page/DetailPage.tsx
+++ components/page/DetailPage.tsx
@@ -1,18 +1,21 @@
 "use client";
 import { useState, useRef } from "react";
 import * as XLSX from "xlsx";
-import { ResultDetail } from "../types/koreanDictionary";
-import WordInfoComponent from "../data/Word_Info";
+import { DictionaryItem, NullWord } from "../types/koreanDictionary";
 import classes from "./DetailPage.module.css";
+import { useRouter } from "next/navigation";
 
 export default function DetailPage() {
-  const [codes, setCodes] = useState<string[]>([]);
-  const [results, setResults] = useState<ResultDetail[] | null>(null);
-
+  const [words, setWords] = useState<string[]>([]);
+  const [results, setResults] = useState<DictionaryItem[] | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [isSearching, setIsSearching] = useState<boolean>(false);
   const abortControllerRef = useRef<AbortController | null>(null);
-  const searchResults: ResultDetail[] | { message: string; word?: string } = [];
+  const [nonResults, setNonResults] = useState<NullWord[] | null>(null);
+  const searchResults: DictionaryItem[] = [];
+  const searchNonResults: NullWord[] = [];
+  const router = useRouter();
+
   const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const uploadedFile = e.target.files?.[0];
     if (uploadedFile) {
@@ -28,30 +31,32 @@ export default function DetailPage() {
         const workbook = XLSX.read(data, { type: "binary" });
         const sheetName = workbook.SheetNames[0];
         const sheet = workbook.Sheets[sheetName];
-        const json = XLSX.utils.sheet_to_json<{ Target: string }>(sheet); // Target 열 읽기
-        const targetList = json.map((row) => row.Target); // Target 열에서 데이터 추출
-        setCodes(targetList);
+        const json = XLSX.utils.sheet_to_json<{ word: string }>(sheet);
+        const wordList = json.map((row) => row.word);
+        setWords(wordList);
       }
     };
     reader.readAsBinaryString(file);
   };
-
+  const detailPage = ()=>{
+    router.push('/detail');
+  }
   const handleSearch = async () => {
     setError(null);
-    // setResults(null);
+    setResults(null);
     setIsSearching(true);
     abortControllerRef.current = new AbortController();
 
-    if (codes.length === 0) {
+    if (words.length === 0) {
       setError("엑셀 파일에서 단어를 불러오세요.");
       setIsSearching(false);
       return;
     }
 
     try {
-      for (const code of codes) {
+      for (const word of words) {
         const response = await fetch(
-          `/api/dictionary-code?code=${encodeURIComponent(code)}`,
+          `/api/korean-dictionary?word=${encodeURIComponent(word)}`,
           {
             method: "GET",
             headers: { "Content-Type": "application/json" },
@@ -64,13 +69,15 @@ export default function DetailPage() {
         }
 
         const data = await response.json();
-        if (data.channel?.item) {
-          const item = data.channel.item;
-          searchResults.push(item); // 객체를 바로 추가
-        } else {
-          console.log(data)
+console.log(data);
+        if (Array.isArray(data.channel?.item)) {
+          searchResults.push(...data.channel.item);
+        }
+        if (!Array.isArray(data.channel?.item)) {
+          searchNonResults.push({ word, message: data.message || "결과 없음" });
         }
       }
+      setNonResults(searchNonResults);
       setResults(searchResults);
     } catch (error) {
       if (abortControllerRef.current.signal.aborted) {
@@ -92,12 +99,14 @@ export default function DetailPage() {
   };
 
   return (
-    <div className={classes.allWrap}>
-      <h1>국립국어원 사전내용 검색</h1>
+    <div className={classes.container}>
+      <div className={classes.contents}>
+      <h1>국립국어원 단어 검색</h1>
+      <button onClick={detailPage}>사전 내용 검색하기</button>
       <input type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
-      {codes.length > 0 && (
+      {words.length > 0 && (
         <p>
-          {codes.length - 1}개의 단어를 불러왔습니다.
+          {words.length - 1}개의 단어를 불러왔습니다.
           {!isSearching ? (
             <button onClick={handleSearch}>검색 시작</button>
           ) : (
@@ -112,39 +121,82 @@ export default function DetailPage() {
       {error && <p style={{ color: "red" }}>{error}</p>}
 
       {/* 동음이의어 정보 출력 */}
-      <div>
-        <table className={classes.result}>
+
+      <table
+        style={{
+          border: "1px solid black",
+          borderCollapse: "collapse",
+          width: "100%",
+        }}
+      >
+        {results && (
           <thead>
             <tr>
-              <th style={{ width: "100px" }}>넘버</th>
-              <th style={{ width: "100px" }}>타겟코드</th>
-              <th style={{ width: "100px" }}>단어</th>
-              <th style={{ width: "100px" }}>품사</th>
-              <th style={{ width: "800px" }}>정의</th>
-              <th style={{ width: "800px" }}>예문</th>
-              <th style={{ width: "200px" }}>동의어</th>
-              {/* <th style={{ border: "1px solid black", padding: "8px" }}>단어 종류</th> */}
-              {/* <th style={{ border: "1px solid black", padding: "8px" }}>발음 정보</th> */}
-              <th style={{ width: "200px" }}>한자</th>
-
-              {/* <th style={{ border: "1px solid black", padding: "8px" }}>원래 정의</th> */}
+              <th style={{ border: "1px solid black", padding: "8px" }}>
+                단어
+              </th>
+              <th style={{ border: "1px solid black", padding: "8px" }}>
+                정의
+              </th>
+              <th style={{ border: "1px solid black", padding: "8px" }}>
+                링크
+              </th>
+              <th style={{ border: "1px solid black", padding: "8px" }}>
+                품사
+              </th>
+              <th style={{ border: "1px solid black", padding: "8px" }}>
+                Target
+              </th>
             </tr>
           </thead>
-          <tbody>
-            {results?.map(
-              (info, index) => (
-                (
-                  <WordInfoComponent
-                    key={index}
-                    wordInfo={info.word_info}
-                    index={index}
-                    code={info.target_code}
-                  />
-                )
-              )
-            )}
-          </tbody>
-        </table>
+        )}
+        <tbody>
+          {results &&
+            results.map((item, index) => (
+              <tr key={index}>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.word}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.sense?.definition || "정의 없음"}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.sense?.link ? (
+                    <a
+                      href={item.sense.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item.sense?.link}
+                    </a>
+                  ) : (
+                    "링크 없음"
+                  )}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.pos || "품사 없음"}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.target_code || "N/A"}
+                </td>
+              </tr>
+            ))}
+          {nonResults &&
+            nonResults.map((item, index) => (
+              <tr key={index}>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.word}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.message || "품사 없음"}
+                </td>
+                {/* <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {item.target_code || "N/A"}
+                </td> */}
+              </tr>
+            ))}
+        </tbody>
+      </table>
       </div>
     </div>
   );
